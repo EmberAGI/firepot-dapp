8c63abc9be49c70c0b2e015c7bb4743f5245108e
Tom Daniel <0xtomdaniel@gmail.com>
2023-08-30 16:29:27 -0600

    Refactored yield vault page to use view model

--- src/features/YieldVault/yieldVault.tsx
+++ src/features/YieldVault/yieldVault.tsx
@@ -1,18 +1,22 @@
 import HeaderBackButton from '../../components/shared/HeaderBackButton/headerBackButton';
 import DepositWithdrawInput from '../../components/shared/DepositWithdrawInput/depositWithdrawInput';
 import styles from './yieldVault.module.scss';
+import useYieldVaultViewModel from './useYieldVaultViewModel';
+import { useEffect, useState } from 'react';
 
 function YieldVault() {
+  const { properties, commands } = useYieldVaultViewModel();
+
   return (
     <>
       <main className={styles.mainBox}>
         <HeaderBackButton title='Yield Vault' />
         <section className={styles.sectionBox}>
           <div className={styles.titleContainer}>
-            <span>Firepot</span>
+            <span>Firepot Finance</span>
             <div className={styles.currencyBox}>
               <img src='src/assets/verify.svg' loading='lazy' alt='' />
-              <span className={styles.percentage}>20.44%</span>
+              <span className={styles.percentage}>{properties.apy}%</span>
               <span className={styles.currency}>APY</span>
             </div>
           </div>
@@ -21,17 +25,22 @@ function YieldVault() {
             <div className={styles.currencySection}>
               <div className={styles.firstSection}>
                 <span className={styles.symbol}>$</span>
-                <span className={styles.value}>432.54</span>
-                <span className={styles.currency}>USD</span>
+                <span className={styles.value}>{properties.vaultStableBalance}</span>
+                <span className={styles.currency}>{properties.stableSymbol}</span>
               </div>
               <div className={styles.interestSection}>
-                <span>32.24 HOTT</span>
+                <span>
+                  {properties.vaultTokenBalance} {properties.tokenSymbol}
+                </span>
                 <div className={styles.percentage}>
-                  <span>+0.24 (1.17%)</span>
+                  <span>
+                    +{properties.vaultReturn.tokenReturn} ({properties.vaultReturn.returnPercentage}%)
+                  </span>
                 </div>
               </div>
             </div>
           </div>
+          {/* TODO: Add this section when we have the data
           <div className={styles.thirdSection}>
             <span className={styles.title}>Personal Metrics</span>
             <div className={styles.subtitleSection}>
@@ -39,68 +48,118 @@ function YieldVault() {
               <span className={styles.pot}>1.3x</span>
             </div>
           </div>
+          */}
         </section>
         <section className={styles.costBox}>
+          {/* TODO: Add this section when we have the data
           <div className={styles.titleContainer}>
-            <span className={styles.title}>Cost Basics</span>
+            <span className={styles.title}>Cost Basis</span>
             <span className={styles.value}>$428.39 USD</span>
           </div>
+          */}
           <div className={styles.switchContainer}>
             <div className={styles.wrapper}>
               <div className={styles.option}>
-                <input className={styles.input} type='radio' name='btn' value='option1' />
+                <input
+                  className={styles.input}
+                  type='radio'
+                  name='btn'
+                  value='option1'
+                  onChange={() => commands.switchVaultTab('deposit')}
+                  checked={properties.vaultTab == 'deposit'}
+                />
                 <div className={styles.btn}>
                   <span className={styles.span}>Deposit</span>
                 </div>
               </div>
               <div className={styles.option}>
-                <input className={styles.input} type='radio' name='btn' value='option2' />
+                <input
+                  className={styles.input}
+                  type='radio'
+                  name='btn'
+                  value='option2'
+                  onChange={() => commands.switchVaultTab('withdraw')}
+                  checked={properties.vaultTab == 'withdraw'}
+                />
                 <div className={styles.btn}>
                   <span className={styles.span}>Withdraw</span>
                 </div>{' '}
               </div>
             </div>
           </div>
-          <DepositWithdrawInput></DepositWithdrawInput>
+          <DepositWithdrawInput
+            moveAction={properties.vaultTab}
+            tokenName='Firepot Finance'
+            stableSymbol={properties.stableSymbol}
+            availableStableBalance={properties.vaultTab == 'deposit' ? properties.availableStableBalance : properties.vaultStableBalance}
+            moveStableAmount={(amount) => commands.updateMoveStableAmount(amount)}
+            tokenSymbol={properties.tokenSymbol}
+            availableTokenBalance={properties.vaultTab == 'deposit' ? properties.availableTokenBalance : properties.vaultTokenBalance}
+            moveTokenAmount={properties.moveTokenAmount}
+            moveTokenPercentage={properties.moveTokenPercentage}
+          ></DepositWithdrawInput>
         </section>
-        <section className={styles.resume}>
-          <div className={styles.container}>
-            <span className={styles.title}>Fees</span>
-            <div className={styles.valuesContainer}>
-              <div className={styles.currencyContainer}>
-                <span className={styles.symbol}>$</span>
-                <span className={styles.value}>1.00</span>
-                <span className={styles.currency}>USD</span>
+        {properties.showTransactionPreview && properties.transactionPreview != null ? (
+          <section className={styles.resume}>
+            <div className={styles.container}>
+              <span className={styles.title}>Subtotal</span>
+              <div className={styles.valuesContainer}>
+                <div className={styles.currencyContainer}>
+                  <span className={styles.symbol}>$</span>
+                  <span className={styles.value}>{properties.transactionPreview!.subtotalStableAmount}</span>
+                  <span className={styles.currency}>{properties.transactionPreview!.stableSymbol}</span>
+                </div>
+                <span className={styles.subtitle}>
+                  {properties.transactionPreview!.subtotalTokenAmount} {properties.transactionPreview!.tokenSymbol}
+                </span>
               </div>
-              <span className={styles.subtitle}>0.36 HOTT</span>
             </div>
-          </div>
-          <div className={styles.container}>
-            <span className={styles.title}>Penalty</span>
-            <div className={styles.valuesContainer}>
-              <div className={styles.currencyContainer}>
-                <span className={styles.symbol}>$</span>
-                <span className={styles.value}>214.14</span>
-                <span className={styles.currency}>USD</span>
+            <div className={styles.container}>
+              <span className={styles.title}>Penalty</span>
+              <div className={styles.valuesContainer}>
+                <div className={styles.currencyContainer}>
+                  <span className={styles.symbol}>$</span>
+                  <span className={styles.value}>{properties.transactionPreview!.penaltyStableAmount}</span>
+                  <span className={styles.currency}>{properties.transactionPreview!.stableSymbol}</span>
+                </div>
+                <span className={styles.subtitle}>
+                  {properties.transactionPreview!.penaltyTokenAmount} {properties.transactionPreview!.tokenSymbol}
+                </span>
               </div>
-              <span className={styles.subtitle}>16.12 HOTT</span>
             </div>
-          </div>
-          <div className={styles.container}>
-            <span className={styles.title}>Total to be Received</span>
-            <div className={styles.valuesContainer}>
-              <div className={styles.currencyContainer}>
-                <span className={styles.symbol}>$</span>
-                <span className={styles.value}>213.15</span>
-                <span className={styles.currency}>USD</span>
+            <div className={styles.container}>
+              <span className={styles.title}>Fees</span>
+              <div className={styles.valuesContainer}>
+                <div className={styles.currencyContainer}>
+                  <span className={styles.symbol}>$</span>
+                  <span className={styles.value}>{properties.transactionPreview!.feesStableAmount}</span>
+                  <span className={styles.currency}>{properties.transactionPreview!.stableSymbol}</span>
+                </div>
+                <span className={styles.subtitle}>
+                  {properties.transactionPreview!.feesTokenAmount} {properties.transactionPreview!.tokenSymbol}
+                </span>
               </div>
-              <span className={styles.subtitle}>23.69 HOTT</span>
             </div>
-          </div>
-        </section>
-        <footer className={styles.footerBox}>
-          <button>Deposit</button>
-        </footer>
+            <div className={styles.container}>
+              <span className={styles.title}>Total {properties.actionLabel}</span>
+              <div className={styles.valuesContainer}>
+                <div className={styles.currencyContainer}>
+                  <span className={styles.symbol}>$</span>
+                  <span className={styles.value}>{properties.transactionPreview!.totalStableAmount}</span>
+                  <span className={styles.currency}>{properties.transactionPreview!.stableSymbol}</span>
+                </div>
+                <span className={styles.subtitle}>
+                  {properties.transactionPreview!.totalTokenAmount} {properties.transactionPreview!.tokenSymbol}
+                </span>
+              </div>
+            </div>
+          </section>
+        ) : null}
+        {properties.showActionButton ? (
+          <footer className={styles.footerBox}>
+            <button>{properties.actionLabel}</button>
+          </footer>
+        ) : null}
       </main>
     </>
   );
